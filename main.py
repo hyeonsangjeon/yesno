80e4553550cd740ebc607ca878c19cc80585eb7b
hyeonsangjeon <wingnut0310@gmail.com>
2023-08-21 15:06:12 +0900

    add finction to caching data

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # app.py
 from flask import Flask, render_template,jsonify, request
 from flask_socketio import SocketIO, emit
+from flask_caching import Cache
 
 import os
 
@@ -11,21 +12,31 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 
 # Construct the file path relative to the script directory
 file_name = os.path.join(script_dir, 'counts.txt')
-#print('CHCHC : ', file_name)
 
 def read_counts_from_file():
-    with open('counts.txt', 'r') as f:
+    with open(file_name, 'r') as f:
         yes_count, no_count = map(int, f.read().split())
     return yes_count, no_count
 
 def write_counts_to_file(yes_count, no_count):
-    with open('counts.txt', 'w') as f:
+    with open(file_name, 'w') as f:
         f.write(f"{yes_count} {no_count}")
+
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+
+def read_counts_from_cache():
+    yes_count = cache.get('yes_count') or 0
+    no_count = cache.get('no_count') or 0
+    return yes_count, no_count
+
+def write_counts_to_cache(yes_count, no_count):
+    cache.set('yes_count', yes_count)
+    cache.set('no_count', no_count)
 
 
 @socketio.on('increment')
 def handle_increment(data):
-    yes_count, no_count = read_counts_from_file()
+    yes_count, no_count = read_counts_from_cache()
     print("data['countId'] : ", data['countId'])
     if data['countId'] == 'yesCount':
         yes_count += 1
@@ -33,19 +44,19 @@ def handle_increment(data):
     else:
         no_count += 1
         emit('update_count', {'countId': data['countId'], 'value': no_count}, broadcast=True)
-    write_counts_to_file(yes_count, no_count)
+    write_counts_to_cache(yes_count, no_count)
 
 
 @socketio.on('reset_counts')
 def handle_reset():
     # 모든 클라이언트에게 카운트를 0으로 리셋
-    write_counts_to_file(0,0)
+    write_counts_to_cache(0, 0)
     emit('update_count', {'countId': 'yesCount', 'value': 0}, broadcast=True)
     emit('update_count', {'countId': 'noCount', 'value': 0}, broadcast=True)
 
 @socketio.on('get_initial_counts')
 def get_initial_counts():
-    yes_count, no_count = read_counts_from_file()
+    yes_count, no_count = read_counts_from_cache()
     print("initial yes count : ", yes_count)
     emit('update_count', {'countId': 'yesCount', 'value': yes_count}, broadcast=True)
     emit('update_count', {'countId': 'noCount', 'value': no_count}, broadcast=True)
